d9b445ff7c5070e85e3f66d1cf06261a5ee1275d
Socodi <58768900+Socodi@users.noreply.github.com>
2021-05-18 11:23:30 +0100

    Initial Upload
    (Upload due to git issues)

---
+++ Wk5 Java/src/AppWindow.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AppWindow {
+    private JLabel lblMessage;
+    private JLabel lblName;
+    private JPanel pnlMain;
+    private JTextField txtName;
+    private JButton btnReset;
+    private JButton btnSubmit;
+
+    public AppWindow() {
+        btnReset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Console output for debugging
+                System.out.println("------------------");
+                System.out.println("btnReset clicked at " + e.getWhen());
+                // End
+
+                // Action
+                lblMessage.setText("Pending...");
+                txtName.setText("");
+            }
+        });
+        btnSubmit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Console output for debugging
+                System.out.println("------------------");
+                System.out.println("btnSubmit clicked");
+                System.out.println("txtName value: " + txtName.getText());
+                // End
+
+                // Action
+                lblMessage.setText("Hello " + txtName.getText());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("AppWindow");
+        frame.setContentPane(new AppWindow().pnlMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(400,400);
+        frame.setVisible(true);
+    }
+}
